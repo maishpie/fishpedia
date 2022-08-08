12c75b8994fb5c9ddb09c5dce5363ef55640802e
maishpie <96499709+maishpie@users.noreply.github.com>
2022-08-08 15:34:49 -0400

    wrapped in IIFE, made public functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,27 @@
-let fishList = [
-  {name: 'Bitterling', location: 'River', time: 'All day'},
-  {name: 'Pale Chub', location: 'River', time: '9am to 4pm'},
-  {name: 'Crucian Carp', location: 'River', time: 'All day'}
-];
+let fishRepository = (function(){
+  let fishList = [
+    {name: 'Bitterling', location: 'River', time: 'All day'},
+    {name: 'Pale Chub', location: 'River', time: '9am to 4pm'},
+    {name: 'Crucian Carp', location: 'River', time: 'All day'}
+  ];
+
+  function add (fish) {
+    fishList.push(fish);
+  }
+
+  function getAll() {
+    return fishList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  }
+
+})();
+
+let fishList = fishRepository.getAll();
 
 fishList.forEach(function(fish){
-  document.write(fish.name + ', location:' + fish.location)
-})
+  document.write('<p>' + fish.name + ', location: ' + fish.location + '<p>');
+});
